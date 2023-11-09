9f3d6465f6578135445391c901de55706f1a3c80
Ahamed A <21102277+athuv@users.noreply.github.com>
2023-11-09 17:05:55 +0530

    style: Add custom colors to Tailwind CSS config

--- src/components/shared/Header/Header.jsx
+++ src/components/shared/Header/Header.jsx
@@ -1,3 +1,7 @@
 export default function Header() {
-  return <header className="bg-yellow-400 font-bold">HEADER</header>;
+  return (
+    <header>
+      <nav></nav>
+    </header>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#f8f8f8',
+        primaryLight: 'EEF1F1',
+        secondary: '#31796d',
+        tertiary: '#ffbb38',
+      },
+    },
   },
   plugins: [],
 };
