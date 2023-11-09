bd429e476d440a7b683889a888d8eea38d4b60cd
Ahamed A <21102277+athuv@users.noreply.github.com>
2023-11-09 17:46:22 +0530

    style: add menu button in navigation bar

--- src/components/shared/Header/Header.jsx
+++ src/components/shared/Header/Header.jsx
@@ -1,7 +1,13 @@
+import { AiOutlineMenu } from 'react-icons/ai';
+
 export default function Header() {
   return (
-    <header>
-      <nav></nav>
+    <header className="flex items-center px-[5vw] h-[8vh] bg-secondary">
+      <nav className="flex items-center">
+        <button>
+          <AiOutlineMenu className="fill-tertiary text-[10vw]" />
+        </button>
+      </nav>
     </header>
   );
 }
