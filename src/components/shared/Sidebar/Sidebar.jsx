2360dee7ffd2f1e6dafd0b1023e20f922cf71bed
Ahamed A <21102277+athuv@users.noreply.github.com>
2023-11-09 21:24:16 +0530

    feat: add home & shop button to sidebar and interactions

--- src/components/shared/Sidebar/Sidebar.jsx
+++ src/components/shared/Sidebar/Sidebar.jsx
@@ -1,4 +1,9 @@
-import { AiOutlineCloseCircle } from 'react-icons/ai';
+import { Link } from 'react-router-dom';
+import {
+  AiOutlineCloseCircle,
+  AiOutlineHome,
+  AiOutlineShop,
+} from 'react-icons/ai';
 import PropTypes from 'prop-types';
 
 export default function Sidebar({ isSidebarOpen, onClick }) {
@@ -11,10 +16,30 @@ export default function Sidebar({ isSidebarOpen, onClick }) {
         ></div>
         <div className=" fixed h-full w-[280px] bg-primary p-3">
           <div className="flex justify-end text-4xl">
-            <AiOutlineCloseCircle
-              onClick={onClick}
-              className="hover:text-tertiary"
-            />
+            <button>
+              <AiOutlineCloseCircle
+                onClick={onClick}
+                className="hover:text-tertiary"
+              />
+            </button>
+          </div>
+          <div className="flex flex-col gap-3 pt-2">
+            <div className="group">
+              <Link
+                to="/"
+                className="flex items-center justify-start gap-3 text-xl group-hover:text-tertiary"
+              >
+                <AiOutlineHome /> Home
+              </Link>
+            </div>
+            <div className="group">
+              <Link
+                to="shop"
+                className="flex items-center justify-start gap-3 text-xl group-hover:text-tertiary"
+              >
+                <AiOutlineShop className="hover:text-tertiary" /> Shop
+              </Link>
+            </div>
           </div>
         </div>
       </aside>
