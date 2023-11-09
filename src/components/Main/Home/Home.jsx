3bf36f3a1b7e86d8580dedc2a9636071217a7056
Ahamed A <21102277+athuv@users.noreply.github.com>
2023-11-09 14:58:37 +0530

    feat: Implement React component structure with sample data

---
+++ src/components/Main/Home/Home.jsx
@@ -0,0 +1,11 @@
+import Category from './Category/Category';
+import Featured from './Featured/Featured';
+
+export default function Home() {
+  return (
+    <>
+      <Category />
+      <Featured />
+    </>
+  );
+}
