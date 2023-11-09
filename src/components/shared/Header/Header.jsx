ba383b807f4056af9b32196ed650ca10b8c4b4f1
Ahamed A <21102277+athuv@users.noreply.github.com>
2023-11-09 18:07:25 +0530

    style: add logo and cart button in nav bar

--- src/components/shared/Header/Header.jsx
+++ src/components/shared/Header/Header.jsx
@@ -1,11 +1,15 @@
-import { AiOutlineMenu } from 'react-icons/ai';
+import { AiOutlineMenu, AiOutlineShoppingCart } from 'react-icons/ai';
 
 export default function Header() {
   return (
-    <header className="flex items-center px-[5vw] h-[8vh] bg-secondary">
-      <nav className="flex items-center">
+    <header className="flex h-[8vh] items-center bg-secondary px-[5vw]">
+      <nav className="flex w-full items-center justify-between text-white">
         <button>
-          <AiOutlineMenu className="fill-tertiary text-[10vw]" />
+          <AiOutlineMenu className="fill-tertiary text-[8vw]" />
+        </button>
+        <div className="font-logoFont text-[8vw]">OdinShop</div>
+        <button>
+          <AiOutlineShoppingCart className="text-[8vw]" />
         </button>
       </nav>
     </header>
