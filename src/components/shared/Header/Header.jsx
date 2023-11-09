7611004ebe0fe3291aa336daab724eede3e6f14b
Ahamed A <21102277+athuv@users.noreply.github.com>
2023-11-09 20:10:34 +0530

    style: add home & shop buttons for `lg` screen in nav bar

--- src/components/shared/Header/Header.jsx
+++ src/components/shared/Header/Header.jsx
@@ -1,17 +1,30 @@
 import PropTypes from 'prop-types';
-import { AiOutlineMenu, AiOutlineShoppingCart } from 'react-icons/ai';
+import {
+  AiOutlineMenu,
+  AiOutlineShoppingCart,
+  AiOutlineHome,
+  AiOutlineShop,
+} from 'react-icons/ai';
 
 export default function Header({ onClick }) {
   return (
     <header className="flex h-[8vh] items-center bg-secondary px-[5vw]">
       <nav className="flex w-full items-center justify-between text-white">
-        <button onClick={onClick}>
-          <AiOutlineMenu className="fill-tertiary text-[8vw]" />
+        <button className="lg:hidden" onClick={onClick}>
+          <AiOutlineMenu className="fill-tertiary text-4xl" />
         </button>
-        <div className="pt-[7px] font-logoFont text-[8vw]">OdinShop</div>
-        <button>
-          <AiOutlineShoppingCart className="text-[8vw]" />
-        </button>
+        <div className="pt-[7px] font-logoFont text-4xl">OdinShop</div>
+        <div className="flex items-center justify-center gap-3">
+          <button className="hidden lg:inline-block">
+            <AiOutlineShop className="text-4xl" />
+          </button>
+          <button className="hidden lg:inline-block">
+            <AiOutlineHome className="text-4xl" />
+          </button>
+          <button>
+            <AiOutlineShoppingCart className="text-4xl" />
+          </button>
+        </div>
       </nav>
     </header>
   );
