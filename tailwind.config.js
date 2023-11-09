384ccfae982d589ea939f0207c1403159f57373b
Ahamed A <21102277+athuv@users.noreply.github.com>
2023-11-09 20:09:07 +0530

    feat: add min-width: 480 screen size

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      screens: {
+        mobile480: '480px',
+      },
       colors: {
         primary: '#f8f8f8',
         primaryLight: 'EEF1F1',
