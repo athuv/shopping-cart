540463a8d47011450e20340b5f1307ff450aa4de
Ahamed A <21102277+athuv@users.noreply.github.com>
2023-11-12 20:00:50 +0530

    style: implement responsive column sizes for category list

--- src/components/Main/Home/Category/Category.jsx
+++ src/components/Main/Home/Category/Category.jsx
@@ -79,7 +79,7 @@ export default function Category() {
     <section>
       <div className="px-6">
         <h1 className="pb-6 text-2xl font-bold ">Category</h1>
-        <div className="grid grid-cols-2 items-center justify-items-center gap-3">
+        <div className="grid grid-cols-2 items-center justify-items-center gap-3 sm:grid-cols-4 xl:grid-cols-8">
           {categories.map((category, index) => (
             <div
               className="group flex flex-col items-center justify-center gap-4 "
