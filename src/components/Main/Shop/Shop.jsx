3bafd5114328ea1b2eaeadacb3ff93426f1e4ebe
Ahamed A <21102277+athuv@users.noreply.github.com>
2023-11-13 16:06:37 +0530

    feat: implement Shop component with contents and pagination

--- src/components/Main/Shop/Shop.jsx
+++ src/components/Main/Shop/Shop.jsx
@@ -1,14 +1,40 @@
 import { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { NavLink, useParams, useOutletContext } from 'react-router-dom';
 import Card from '../../shared/Card/Card';
 
+function Pagination({ datalength }) {
+  const totalPages = Math.ceil(datalength / 8);
+
+  return (
+    <div className="flex gap-2 px-6 pt-6">
+      {Array.from({ length: totalPages }, (_, index) => (
+        <NavLink
+          className={({ isActive }) =>
+            `bg-secondary p-2 ${isActive ? 'text-tertiary' : ''}`
+          }
+          key={index}
+          to={`/shop/${index + 1}`}
+        >
+          {index + 1}
+        </NavLink>
+      ))}
+    </div>
+  );
+}
+
 export default function Shop() {
-  const [shopData, setShopData] = useState(null);
+  const [shopData, setShopData] = useState([]);
+  const [currentData, setCurrentData] = useState([]);
+  const { page } = useParams();
+
+  const isSidebarOpen = useOutletContext();
 
   useEffect(() => {
-    if (shopData === null) {
+    if (shopData.length === 0) {
       const fetchShopData = async () => {
         const response = await fetch(
-          'https://fakestoreapi.com/products?limit=12',
+          'https://fakestoreapi.com/products?limit=24',
         );
         const data = await response.json();
         setShopData(data);
@@ -17,11 +43,38 @@ export default function Shop() {
       fetchShopData();
     }
   });
+
+  useEffect(() => {
+    if (shopData.length > 0) {
+      const currentPage = page ? parseInt(page, 10) : 1;
+
+      const itemsPerPage = 8;
+
+      const startIndex = (currentPage - 1) * itemsPerPage;
+      const endIndex = startIndex + itemsPerPage;
+
+      const currentItems = shopData.slice(startIndex, endIndex);
+      setCurrentData(currentItems);
+    }
+  }, [page, shopData]);
+
   return (
-    <section>
+    <section className="flex flex-col items-center">
+      {shopData.length > 0 && <Pagination datalength={shopData.length} />}
+
       <div className="px-6">
-        <Card title={'Available Products'} data={shopData} />
+        <Card
+          title={'Available Products'}
+          isSidebarOpen={isSidebarOpen}
+          data={currentData}
+        />
       </div>
+
+      {shopData !== null && <Pagination datalength={shopData.length} />}
     </section>
   );
 }
+
+Pagination.propTypes = {
+  datalength: PropTypes.number.isRequired,
+};
