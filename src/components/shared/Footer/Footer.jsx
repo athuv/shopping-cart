c0d6c0fa68c2e811c1eb273ddd611a69c53d27b0
Ahamed A <21102277+athuv@users.noreply.github.com>
2023-11-13 11:52:49 +0530

    style: design footer component with relevant contents

--- src/components/shared/Footer/Footer.jsx
+++ src/components/shared/Footer/Footer.jsx
@@ -1,3 +1,22 @@
+import { BsInstagram, BsTwitter, BsFacebook } from 'react-icons/bs';
+
 export default function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="flex justify-between gap-4 bg-secondary p-4">
+      <div className="flex gap-2">
+        <div>
+          <BsInstagram className="text-4xl text-white" />
+        </div>
+        <div>
+          <BsTwitter className="text-4xl text-white" />
+        </div>
+        <div>
+          <BsFacebook className="text-4xl text-white" />
+        </div>
+      </div>
+      <div className="text-white">
+        <span className="flex">All Rights Reserved 2023</span>
+      </div>
+    </footer>
+  );
 }
