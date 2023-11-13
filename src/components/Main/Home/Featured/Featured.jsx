43b4f69ef041dd967ad116c2c25ee00a15d66c36
Ahamed A <21102277+athuv@users.noreply.github.com>
2023-11-13 11:17:44 +0530

    style: change grid columns according to screen size

--- src/components/Main/Home/Featured/Featured.jsx
+++ src/components/Main/Home/Featured/Featured.jsx
@@ -22,7 +22,7 @@ export default function Featured() {
     <section>
       <div className="px-6">
         <h1 className="pb-6 text-2xl font-bold ">Featured Products</h1>
-        <div className="grid grid-cols-1 gap-6">
+        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
           {featuredData === null ? (
             <div>
               <img
