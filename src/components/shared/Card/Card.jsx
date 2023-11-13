88282931c0c3073e79bcc7cf421ee453eef669bc
Ahamed A <21102277+athuv@users.noreply.github.com>
2023-11-13 12:46:47 +0530

    feat: add `Add to Cart` button to Card component

--- src/components/shared/Card/Card.jsx
+++ src/components/shared/Card/Card.jsx
@@ -1,5 +1,7 @@
 import loadingCircle from './../../../assets/images/loading-circles.svg';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { TbShoppingCartPlus } from 'react-icons/tb';
 
 export default function Card({ featuredData }) {
   return (
@@ -18,7 +20,7 @@ export default function Card({ featuredData }) {
           featuredData.map((data) => (
             <div
               key={data.id}
-              className="flex h-[480px] w-60 flex-col items-center justify-center gap-2 justify-self-center bg-primaryLight p-[2%]"
+              className="group relative flex h-[480px] w-60 flex-col items-center justify-center gap-2 justify-self-center overflow-hidden bg-primaryLight p-[2%]"
             >
               <div className="flex flex-grow flex-col items-center justify-center">
                 <img
@@ -30,6 +32,11 @@ export default function Card({ featuredData }) {
               <div className="flex flex-col items-center justify-center">
                 <div className="line-clamp-2 text-center">{data.title}</div>
                 <div className="text-center">{data.price}</div>
+              </div>
+              <div className="flex w-full translate-y-10 items-center justify-center transition duration-500 group-hover:-translate-y-32">
+                <Link className="absolute flex  items-center  justify-center gap-1 rounded bg-secondary p-2 text-white">
+                  <TbShoppingCartPlus /> <span>Add to Cart</span>
+                </Link>
               </div>
             </div>
           ))
