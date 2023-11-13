88254ddeb9fdd601e0b2ddf7ecc1dadc7bf72576
Ahamed A <21102277+athuv@users.noreply.github.com>
2023-11-13 11:04:44 +0530

    feat: add featured component to home page
    - Fetch data from API
    - Style component
    - Add loading image until data fetches

--- src/components/Main/Home/Featured/Featured.jsx
+++ src/components/Main/Home/Featured/Featured.jsx
@@ -1,3 +1,58 @@
+import { useEffect, useState } from 'react';
+import loadingCircle from './../../../../assets/images/loading-circles.svg';
+
 export default function Featured() {
-  return <div>Featured</div>;
+  const [featuredData, setFeaturedData] = useState(null);
+
+  useEffect(() => {
+    if (featuredData === null) {
+      const fetchFeaturedData = async () => {
+        const response = await fetch(
+          'https://fakestoreapi.com/prosductss?limit=6',
+        );
+        const data = await response.json();
+        setFeaturedData(data);
+      };
+
+      fetchFeaturedData();
+    }
+  });
+
+  return (
+    <section>
+      <div className="px-6">
+        <h1 className="pb-6 text-2xl font-bold ">Featured Products</h1>
+        <div className="grid grid-cols-1 gap-6">
+          {featuredData === null ? (
+            <div>
+              <img
+                className="h-16 w-full lg:h-28"
+                alt="loading-circle"
+                src={loadingCircle}
+              />
+            </div>
+          ) : (
+            featuredData.map((data) => (
+              <div
+                key={data.id}
+                className="flex h-[480px] w-60 flex-col items-center justify-center gap-2 bg-primaryLight p-[2%]"
+              >
+                <div className="flex flex-grow flex-col items-center justify-center">
+                  <img
+                    className="h-fit w-fit"
+                    alt={data.title}
+                    src={data.image}
+                  />
+                </div>
+                <div className="flex flex-col items-center justify-center">
+                  <div className="line-clamp-2 text-center">{data.title}</div>
+                  <div className="text-center">{data.price}</div>
+                </div>
+              </div>
+            ))
+          )}
+        </div>
+      </div>
+    </section>
+  );
 }
